2ae6b33c6e14b5a7f4764a1fa9e252655567225b
Sujith M <sujithm1610@gmail.com>
2023-10-01 06:36:14 +0530

    setup done

---
+++ src/NLPtc/constants/__init__.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+CONFIG_FILE_PATH = Path("D:\model deploy end to end\NLP-text_classification\config\config.yaml")
+PARAMS_FILE_PATH = Path("D:\model deploy end to end\NLP-text_classification\params.yaml")
+SCHEMA_FILE_PATH = Path("D:\model deploy end to end\NLP-text_classification\schema.yaml")
